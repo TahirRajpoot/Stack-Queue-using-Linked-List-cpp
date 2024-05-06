0ea509aed6ea7ae232844386ca7acfcfe1e8a660
Muhammad Tahir <ranat5177@gmail.com>
2024-05-06 23:51:06 +0500

    Add Queue with LinkedList

---
+++ QueueWithLinkedList.cpp
@@ -0,0 +1,256 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+
+    public:
+        int Data;
+        Node* Next;
+        Node(int key)
+        {
+            this->Data = key;
+            this->Next  = NULL;
+        }
+    
+};
+class LinkedList
+{
+    private:
+        Node*Head;
+    public:
+        LinkedList()
+        {
+            Head = NULL;
+        }
+        void InsertHead(int key)
+        {
+            Node*N = new Node(key);
+            if(Head != NULL)
+            {
+                cout<<"Successfully Inserted at Head"<<endl;
+               N->Next = Head;   
+            }
+            Head = N;
+        }
+        Node* Search(int key)
+        {
+            Node*p = Head;
+            while(p != NULL )
+            {
+                if(p->Data == key)
+                {
+                    return p;
+                }
+                else
+                {
+                    p = p->Next;
+                }
+            }
+            return NULL;
+        }
+        void InsertTail(int key)
+        {
+            if(Head != NULL)
+            {
+                Node*p = Head;
+                Node*N = new Node(key);
+                while( p->Next != NULL )
+                {
+                    p = p->Next;
+                }
+                p->Next = N;
+            }
+        }
+        void print()
+        {
+            Node* p = Head;
+            if (Head == NULL) 
+            {
+                cout << "Linked List is Empty" << endl;
+                return;
+            }
+            while (p != NULL) {
+                cout << p->Data << " ";
+                p = p->Next;
+            }
+        }
+        void Delete(int key)
+        {
+            Node*p = Head;
+            Node*previous = Head;
+            if(p->Data == key)
+            {
+                Head = p->Next;
+            }
+            while(p != NULL)
+            {
+                if(p->Data == key)
+                {
+                    previous->Next = p->Next;
+                    break;
+                }
+                previous = p;
+                p = p->Next;
+            }
+        }
+        void DeleteHead()
+        {
+            if(Head != NULL)
+            {
+                Head = Head->Next;
+            }
+        }
+        bool Insert(int key , int Loc)
+        {
+            int count = 0;
+            if(Loc == 0)
+            {
+                InsertHead(key);
+                return true;
+            }
+            Node*p = Head;
+            Node*previous = Head;
+            Node*N = new Node(key);
+            while(p != NULL)
+            {
+                if(count == Loc)
+                {
+                    previous->Next = N;
+                    N->Next = p;
+                    return true;
+                }
+                previous = p;
+                count = count + 1;
+                p = p->Next;
+            }
+            return false;
+        }
+        bool Update(int key , int Loc)
+        {
+            int count = 0;
+            Node*p = Head;
+            
+            while(count != Loc)
+            {
+                p = p->Next;
+                count = count + 1;
+            }
+            if(p != NULL)
+            {
+                p->Data = key;
+                cout<<"Successfully Updated at "<<Loc<<endl;
+                return true;
+            }
+            cout<<"Index Not Found"<<endl;
+            return false;
+        }
+        int GetHeadVlaue()
+        {
+            if(Head != NULL)
+            {
+                int x = Head->Data;
+                return x;
+            }
+        }
+        Node* GetHead()
+        {
+            return Head;
+        }
+        void RemoveFromTail()
+        {
+            Node*p = Head;
+            Node*previous = Head;
+            if(p->Next == NULL)
+            {
+                Head = NULL;
+            }
+            while(p != NULL)
+            {
+                if(p->Next == NULL)
+                {
+                    cout<<p->Data<<" Deleted";
+                    previous->Next = NULL;
+                    break;
+                }
+                previous = p;
+                p = p->Next;
+            }
+        }
+        int GetTailValue()
+        {
+            Node*p = Head;
+            Node*previous = Head;
+            if(p->Next == NULL)
+            {
+                int x = p->Data;
+                return x;
+            }
+            while(p != NULL)
+            {
+                if(p->Next == NULL)
+                {
+                    
+                    int x = p->Data;
+                    return x;
+                    
+                }
+                previous = p;
+                p = p->Next;
+            }
+        }
+};
+
+class QueueWithLinkList
+{
+private:
+    LinkedList a;
+public:
+    QueueWithLinkList()
+    {
+
+    }
+    void ENQUEUE(int x)
+    {
+        a.InsertHead(x);
+    }
+    void DEQUEUE()
+    {
+        a.RemoveFromTail();
+    }
+    int PEEK()
+    {
+        if(a.GetHead() != NULL)
+        {
+            int x = a.GetTailValue();
+            return x;
+        }
+    }
+    bool IsEmpty()
+    {
+        if(a.GetHead() == NULL)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+};
+
+
+int main()
+{
+    
+    QueueWithLinkList a;
+    a.ENQUEUE(9);
+    a.ENQUEUE(10);
+    a.ENQUEUE(12);
+    int x = a.PEEK();
+    cout<<a.IsEmpty();
+    
+}
+
+    
